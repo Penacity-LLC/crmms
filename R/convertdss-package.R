5d176160896ce333da5f2991ae1cb82e58beb128
cbracken-usbr <cbracken@usbr.gov>
2015-04-14 11:00:37 -0600

    Better package description

--- R/convertdss-package.R
+++ R/convertdss-package.R
@@ -1,5 +1,14 @@
-#' convertdss.
+#' convertdss: A package for converting dss into other data formats
 #'
-#' @name convertdss
+#' The convertdss package provides functionality to convert dss files into:
+#' NetCDF, SQLite.
+#' 
+#' @section NetCDF:
+#' About NetCDF conversion
+#'
+#' @section SQLite:
+#' About SQLite conversion
+#'
 #' @docType package
+#' @name convertdss
 NULL
